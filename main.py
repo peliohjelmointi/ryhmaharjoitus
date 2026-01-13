f59f62e4aacf50d6e8f08223d9d6cac1558df8d0
Kari Perho <151856768+kariperho@users.noreply.github.com>
2026-01-13 12:58:18 +0200

    Update main.py
    Added some code documentation

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+"""Adding some code documentation in this update"""
+
+# Define the main function
 def main():
   pass
 
+# Auxiliary function
 def second():
   pass
 
+# Print something
 def a_function():
   print("This had to be printed.")
